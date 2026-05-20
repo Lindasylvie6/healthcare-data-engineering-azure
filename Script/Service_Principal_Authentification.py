66e5ef5101e3768e845a1b6cce31e0ec1bb94c96
Sylvie Linda <166336236+Lindasylvie6@users.noreply.github.com>
2026-05-20 13:14:36 -0400

    Refactor Service Principal authentication for ADLS Gen2
    Updated storage account name and paths for ADLS Gen2.

--- Script/Service_Principal_Authentification.py
+++ Script/Service_Principal_Authentification.py
@@ -1,27 +1,27 @@
-# Configuration (Service Principal Authentication)
-storage_account_name = "hospitaldwh"
+# Configuration (Service Principal + ADLS Gen2)
+storage_account_name = "adlshospitaldwh"
+container_landing = "landing"
+container_bronze = "bronze"
 container_silver = "silver"
 container_gold = "gold"
-container_bronze = "bronze"
-container_landing = "landing"
 
 # Service Principal credentials
 application_id = "ad3497a3-a8be-4251-8e01-4b7bc1c781b4"
 tenant_id = "2be11f7b-4e63-4e64-b8a4-992816fe3359"
-client_secret = "r.Z8Q~dtPdES3m64J_CgHnb1-AsUfe6L8tDBma4f"
+client_secret = "***************************************"
 
-# Service Principal with Blob Storage
-spark.conf.set(f"fs.azure.account.auth.type.{storage_account_name}.blob.core.windows.net", "OAuth")
-spark.conf.set(f"fs.azure.account.oauth.provider.type.{storage_account_name}.blob.core.windows.net", "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider")
-spark.conf.set(f"fs.azure.account.oauth2.client.id.{storage_account_name}.blob.core.windows.net", application_id)
-spark.conf.set(f"fs.azure.account.oauth2.client.secret.{storage_account_name}.blob.core.windows.net", client_secret)
-spark.conf.set(f"fs.azure.account.oauth2.client.endpoint.{storage_account_name}.blob.core.windows.net", f"https://login.microsoftonline.com/{tenant_id}/oauth2/token")
+# Configure Service Principal authentication
+spark.conf.set(f"fs.azure.account.auth.type.{storage_account_name}.dfs.core.windows.net", "OAuth")
+spark.conf.set(f"fs.azure.account.oauth.provider.type.{storage_account_name}.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider")
+spark.conf.set(f"fs.azure.account.oauth2.client.id.{storage_account_name}.dfs.core.windows.net", application_id)
+spark.conf.set(f"fs.azure.account.oauth2.client.secret.{storage_account_name}.dfs.core.windows.net", client_secret)
+spark.conf.set(f"fs.azure.account.oauth2.client.endpoint.{storage_account_name}.dfs.core.windows.net", f"https://login.microsoftonline.com/{tenant_id}/oauth2/token")
 
-# Paths
-LANDING_PATH = f"wasbs://{container_landing}@{storage_account_name}.blob.core.windows.net/"
-BRONZE_PATH = f"wasbs://{container_bronze}@{storage_account_name}.blob.core.windows.net/"
-SILVER_PATH = f"wasbs://{container_silver}@{storage_account_name}.blob.core.windows.net/"
-GOLD_PATH = f"wasbs://{container_gold}@{storage_account_name}.blob.core.windows.net/"
+# Paths using abfss://
+LANDING_PATH = f"abfss://{container_landing}@{storage_account_name}.dfs.core.windows.net/"
+BRONZE_PATH = f"abfss://{container_bronze}@{storage_account_name}.dfs.core.windows.net/"
+SILVER_PATH = f"abfss://{container_silver}@{storage_account_name}.dfs.core.windows.net/"
+GOLD_PATH = f"abfss://{container_gold}@{storage_account_name}.dfs.core.windows.net/"
 
 print("Configuration complete")
 print(f"Landing path: {LANDING_PATH}")
